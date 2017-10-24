77f56fc1ee2e255a38c557e5b07e3fb0131dec5c
Yebin Yu <yuyaebean@naver.com>
2017-10-24 13:48:08 +0900

    add HW06

---
+++ HW06/slideshow.js
@@ -0,0 +1,19 @@
+var slideIndex = 1;
+
+function changeSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+}
