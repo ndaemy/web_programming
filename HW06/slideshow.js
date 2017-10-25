f46ff1116f1c6192d64494baff7129492a2a8c17
Yebin Yu <yuyaebean@naver.com>
2017-10-25 21:57:52 +0900

    modify HW06

--- HW06/slideshow.js
+++ HW06/slideshow.js
@@ -1,19 +1,23 @@
-var slideIndex = 1;
+var idx = 1;
 
-function changeSlide(n) {
-  showSlide(slideIndex = n);
-}
+$(function() {
+  $(".indicator > ul > li").click(function(e) {
+    var $item = $(e.currentTarget);
+    idx = $item.index() + 1;
+    $(".indicator > ul > li").removeClass('active');
+    $(".slideshow > img").removeClass('active');
+    $item.addClass('active');
+    $(".slideshow > img:nth-child(" + idx + ")").addClass('active');
+  });
+  setInterval("autoIncrease()", 3000);
+});
 
-function showSlide(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = slides.length;}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+function autoIncrease() {
+  idx++;
+  if (idx > 5)
+    idx -= 5;
+  $(".indicator > ul > li").removeClass('active');
+  $(".slideshow > img").removeClass('active');
+  $(".indicator > ul > li:nth-child(" + idx + ")").addClass('active');
+  $(".slideshow > img:nth-child(" + idx + ")").addClass('active');
 }
